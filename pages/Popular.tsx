9ace5c8aeb21509e34c93594671dc3a832306f4c
gael <cgvalenzuelac@concredito.com.mx>
2021-03-19 14:46:23 -0700

    .

--- pages/Popular.tsx
+++ pages/Popular.tsx
@@ -1,6 +1,10 @@
+import { Container } from "react-bootstrap"
 
 
 export default () => {
     
-    return (<h3>Populares</h3>);
+    return (<Container>
+        <h3>Populares</h3>
+        <span>colocar los shows por paguinado e ir cambiando segun la paguina mostrada, agregar parametro query para seleccionar paguina</span>
+        </Container>);
 }
--- pages/Top.tsx
+++ pages/Top.tsx
@@ -1,6 +1,11 @@
+import { Container } from "react-bootstrap";
 
 
 export default () => {
     
-    return (<h3>Top</h3>);
+        
+    return (<Container>
+        <h3>Top</h3>
+        <span>colocar los shows por paguinado e ir cambiando segun la paguina mostrada, agregar parametro query para seleccionar paguina</span>
+        </Container>);
 }
